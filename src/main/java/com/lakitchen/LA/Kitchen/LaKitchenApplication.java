1b5f7ab14550a80a54b5e1ac05a5fb49e9cc539b
Albert <=>
2021-01-01 21:57:07 +0700

    Setup project

---
+++ src/main/java/com/lakitchen/LA/Kitchen/LaKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.lakitchen.LA.Kitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaKitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakitchen/LA/Kitchen/LaKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakitchen.LA.Kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
