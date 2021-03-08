c7fa9081097976226bd217c1faca6c789a616ae3
Albert <=>
2021-03-09 03:23:13 +0700

    Add new config

---
+++ src/main/java/com/lakitchen/LA/Kitchen/configuration/LocaleConfig.java
@@ -0,0 +1,16 @@
+package com.lakitchen.LA.Kitchen.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@Configuration
+public class LocaleConfig {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+}
--- src/main/java/com/lakitchen/LA/Kitchen/configuration/WebsocketConfiguration.java
+++ src/main/java/com/lakitchen/LA/Kitchen/configuration/WebsocketConfiguration.java
@@ -1,5 +1,6 @@
 package com.lakitchen.LA.Kitchen.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,10 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${app.frontendBaseUrl}")
+    String frontendURL;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("http://localhost:8081")
+                .setAllowedOrigins(this.frontendURL)
                 .withSockJS();
     }
 
