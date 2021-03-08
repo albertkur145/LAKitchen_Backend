a7d3eee6e46943475b3ea89e85b7119babc8bc7e
Albert <=>
2021-03-09 05:39:03 +0700

    New config

--- src/main/java/com/lakitchen/LA/Kitchen/configuration/WebConfiguration.java
+++ src/main/java/com/lakitchen/LA/Kitchen/configuration/WebConfiguration.java
@@ -1,6 +1,5 @@
 package com.lakitchen.LA.Kitchen.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -10,14 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
-    @Value("${app.frontendBaseUrl}")
-    String frontendURL;
-
     private final long MAX_AGE_SECS = 3600;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(MAX_AGE_SECS);
